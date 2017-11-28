c06fb88517050cc1396efb13799760df7016c9dd
Michael Evans <michael@michaelgregoryevans.com>
2017-11-28 18:20:50 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     self.find_by(rating: self.highest_rating)
   end
+
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    self.find_by(rating: self.lowest_rating)
+  end
 end

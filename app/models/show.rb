1a23094a98969280f8f581d58e65f4283a464a78
Michael Evans <michael@michaelgregoryevans.com>
2017-11-28 18:20:00 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,10 @@
 class Show < ActiveRecord::Base
 
   def self.highest_rating
-    Show.maximum(:rating)
+    self.maximum(:rating)
   end
 
   def self.most_popular_show
-    Show.find_by(rating: Show.highest_rating)
+    self.find_by(rating: self.highest_rating)
   end
 end
